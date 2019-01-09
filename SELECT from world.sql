0bb126a4d3dc72289f8b542e8f644c8bbfbe0da1
Hubert Rośniak <39415438+Hesuroki@users.noreply.github.com>
2019-01-09 19:19:14 +0100

    Update SELECT from world.sql

--- SELECT from world.sql
+++ SELECT from world.sql
@@ -9,7 +9,7 @@ Introduction
 Observe the result of running this SQL command to show the name, continent and population of all countries. */
 
 
-SELECT name, continent, population FROM world
+SELECT name, continent, population FROM world;
 
 /* 2.
 Large Countries
